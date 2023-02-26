830054db1ba282d11bc83d72c312765d0530554a
Nicholas Galante <nicholasgalante94@gmail.com>
2023-02-26 16:22:48 -0500

    commit tree e904a64e26d0b98f1bff199af4be6dd6ee8c2e3f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker name
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+   newArray = []
+   names.each do |name|
+      newArray << badge_maker(name)
+   end   
+   newArray
+end
+
+def assign_rooms speakers
+   newArray = []
+   speakers.each_with_index do |speaker, index|
+      newArray << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+   end
+   newArray
+end
+
+def printer names
+   badges = batch_badge_creator(names)
+   rooms = assign_rooms(names)
+   badges.each { |name| puts name }
+   rooms.each {|name| puts name }
+end
